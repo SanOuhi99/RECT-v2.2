c38cc00ce4218e9bcdc58629fc058ae3b7363df6
SanOuhi99 <sanouhi99@gmail.com>
2025-07-19 20:20:39 +0100

    Update RunMatchingButton.jsx

--- frontend/src/components/dashboard/RunMatchingButton.jsx
+++ frontend/src/components/dashboard/RunMatchingButton.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useSWRConfig } from 'swr';
-import { CheckCircleIcon, ExclamationCircleIcon, RefreshIcon } from '@heroicons/react/outline';
+import { CheckCircleIcon, ExclamationCircleIcon, RefreshIcon } from '@heroicons/react/24/outline'
 import { useAuth } from '../../../contexts/AuthContext';
 
 export default function RunMatchingButton() {
