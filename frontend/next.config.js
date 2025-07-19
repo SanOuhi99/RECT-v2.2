9b24d46838e447464f7d47fe28e04ffe60a369b6
SanOuhi99 <sanouhi99@gmail.com>
2025-07-19 21:41:06 +0100

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
   webpack: (config) => {
-    config.resolve.extensions = ['.js', '.jsx', '.ts', '.tsx', ...config.resolve.extensions];
+    config.resolve.alias['@'] = path.join(__dirname, 'src');
     return config;
   },
-};
+}
 
-module.exports = nextConfig;
+const path = require('path');
+
+module.exports = nextConfig
