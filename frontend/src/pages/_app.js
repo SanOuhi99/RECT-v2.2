eb5c520a92d474a40fc316a4155decb1f32d0376
SanOuhi99 <sanouhi99@gmail.com>
2025-07-19 18:12:01 +0100

    Update _app.js

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,5 +1,14 @@
 import { AuthProvider } from '../contexts/AuthContext';
 import '../styles/globals.css';
+import * as Sentry from '@sentry/nextjs';
+
+if (process.env.NEXT_PUBLIC_SENTRY_DSN) {
+  Sentry.init({
+    dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+    tracesSampleRate: 0.1,
+    environment: process.env.NODE_ENV,
+  });
+}
 
 function MyApp({ Component, pageProps }) {
   return (
