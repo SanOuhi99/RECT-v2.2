83f0c5ba98f355c1b5484005de4e17e4c1174e6d
SanOuhi99 <sanouhi99@gmail.com>
2025-07-19 20:32:23 +0100

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,9 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+  webpack: (config) => {
+    config.resolve.extensions.push('.jsx', '.js');
+    return config;
+  },
 };
 
 module.exports = nextConfig;
