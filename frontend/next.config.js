9745e27bb4cddc2f6aadb84e39b686caafa9682b
SanOuhi99 <sanouhi99@gmail.com>
2025-07-19 20:04:41 +0100

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,24 +1,8 @@
-module.exports = {
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-  productionBrowserSourceMaps: false,
-  optimizeFonts: true,
-  output: 'standalone',
-  compress: true,
-  env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://rect.up.railway.app:8080',
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
-      },
-    ]
-  },
-  experimental: {
-      outputFileTracingRoot: path.join(__dirname, '../../')
-    },
-  images: {
-    domains: ['rect.up.railway.app'],
-  },
-}
+  swcMinify: true,
+};
+
+module.exports = nextConfig;
