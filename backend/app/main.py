fa93d29276ee8255c286a4db1524cf60b9702437
SanOuhi99 <sanouhi99@gmail.com>
2025-07-19 17:50:36 +0100

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,12 +10,7 @@ from slowapi import Limiter
 from slowapi.util import get_remote_address
 
 limiter = Limiter(key_func=get_remote_address)
-middleware = [
-    Middleware(
-        TrustedHostMiddleware,
-        allowed_hosts=["example.com", "*.example.com"]
-    )
-]
+middleware = [Middleware(SlowAPIMiddleware)]
 
 app = FastAPI(middleware=middleware)
 app.state.limiter = limiter
